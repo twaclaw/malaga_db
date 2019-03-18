9fb086e6de4bdeadb6a33d0e15a42ad07eb5c846
vantage <dsandovalv@gmail.com>
2019-03-18 12:19:47 -0500

    Added configuration scripts

---
+++ scripts/transfer_db.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+REPO=$HOME/repo_db
+DB=/var/lib/weewx/weewx.sdb 
+
+
+cp  $DB $REPO
+cd $REPO && git add weewx.sdb && git commit -m "Update database" && git push -u origin master
+
+exit 0

3f04f1b7d080b4df7910c4540b8b780ca8645cdb
vantage <dsandovalv@gmail.com>
2019-03-19 01:15:32 -0500

    Updated scripts: non-functional changes

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -3,8 +3,12 @@
 REPO=$HOME/repo
 WWW=/var/www/html/weewx
 
-
 cp -r $WWW/* $REPO
-cd $REPO && rm -rf .git && git init && git add README.md icons/ images/ mobile/ *.html *.png *.xml *.js *.css favicon.ico NOAA font && git commit -m "Added generated web files" && git remote add origin git@github.com:twaclaw/malaga.git && git push -u --force origin master
+cd $REPO\
+&& rm -rf .git\
+&& git init && git add README.md icons/ images/ mobile/ *.html *.png *.xml *.js *.css favicon.ico NOAA font\
+&& git commit -m "Added generated web files"\
+&& git remote add origin git@github.com:twaclaw/malaga.git\
+&& git push -u --force origin master
 
 exit 0
--- scripts/transfer_db.sh
+++ scripts/transfer_db.sh
@@ -5,6 +5,9 @@ DB=/var/lib/weewx/weewx.sdb
 
 
 cp  $DB $REPO
-cd $REPO && git add weewx.sdb && git commit -m "Update database" && git push -u origin master
+cd $REPO\
+&& git add weewx.sdb\
+&& git commit -m "Update database"\
+&& git push -u origin master
 
 exit 0

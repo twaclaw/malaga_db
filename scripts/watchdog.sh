5f96f9daf460320e891c680e4027089d400e5832
vantage <dsandovalv@gmail.com>
2019-06-01 10:29:11 -0500

    Added script to verify that the displayed webpage is up-to-date

---
+++ scripts/watchdog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LOG=$HOME/log.out 
+
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>$LOG 2>&1
+
+WEB=https://twaclaw.github.io/malaga/ 
+
+# dd/mm/yy
+webdate=$(curl -s $WEB |grep lastupdate |grep -Eo '[0-9]{2}/[0-9]{2}/[0-9]{2}')
+WEB_DAY=$(echo $webdate | cut -d '/' -f 1 )
+CURRENT_DAY=$(date '+%d')
+
+if [ "$WEB_DAY" -ne "$CURRENT_DAY" ]
+then
+    echo "$(date) Date mistmatch, dumping console data and clearing memory"
+    sudo /etc/init.d/weewx stop
+    wee_device --dump
+    wee_device --clear-memory
+    sudo /etc/init.d/weewx start
+fi
+exit 0

2875a4aa9b87afd07675c78e890a89b1a8b8b3e5
Diego Sandoval <46681084+twaclaw@users.noreply.github.com>
2019-06-01 16:38:22 +0200

    Added git pull
    Sync with possible external changes

--- scripts/transfer_db.sh
+++ scripts/transfer_db.sh
@@ -6,6 +6,7 @@ DB=/var/lib/weewx/weewx.sdb
 
 cp  $DB $REPO
 cd $REPO\
+&& git pull --no-edit origin master\
 && git add weewx.sdb\
 && git commit -m "Update database"\
 && git push -u origin master

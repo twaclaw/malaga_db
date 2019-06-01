dd0f20305cbaecd98dee94cac1635be108e72c34
vantage <dsandovalv@gmail.com>
2019-06-01 10:46:42 -0500

    Added dates in watchdog error message

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -2,9 +2,9 @@
 
 LOG=$HOME/log.out 
 
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-#exec 1>$LOG 2>&1
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$LOG 2>&1
 
 WEB=https://twaclaw.github.io/malaga/ 
 
@@ -15,7 +15,7 @@ CURRENT_DAY=$(date '+%d')
 
 if [ "$WEB_DAY" -ne "$CURRENT_DAY" ]
 then
-    echo "$(date) Date mistmatch, dumping console data and clearing memory"
+    echo "$(date) Date mistmatch, current day: $CURRENT_DAY, web day: $WEB_DAY. Dumping console data and clearing memory"
     sudo /etc/init.d/weewx stop
     wee_device --dump
     wee_device --clear-memory

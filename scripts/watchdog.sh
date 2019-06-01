1821eb051f6d6389c972028c62464c9fe4409f25
Diego Sandoval <46681084+twaclaw@users.noreply.github.com>
2019-06-01 17:33:18 +0200

    Update watchdog.sh

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -2,9 +2,9 @@
 
 LOG=$HOME/log.out 
 
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-#exec 1>$LOG 2>&1
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$LOG 2>&1
 
 WEB=https://twaclaw.github.io/malaga/ 
 
